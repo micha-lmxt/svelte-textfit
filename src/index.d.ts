cd1400e90abc67cb8f0a33b09bf154d2a8366439
micha-lmxt <michael@gradientdescent.de>
2021-03-10 14:37:52 +0100

    make props optional

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,57 +2,57 @@ interface TextfitProps {
     /**
      * An update element, change in this element will trigger an update
      */
-    update: any,
+    update?: any,
     /**
      * Minimum value for interpolation
      * Default: 1
      */
-    min: number,
+    min?: number,
     /**
      * Maximum value for interpolation
      * Default 100
      */
-    max: number,
+    max?: number,
     /**
      *  (single|multi) Algorithm to fit the text. Use single for headlines and multi for paragraphs. 
      *  Default is multi.
      */
-    mode: "multi" | "single",
+    mode?: "multi" | "single",
     /**
      * (Boolean) When mode is single and forceSingleModeWidth is true, the element's height will be ignored. 
      * Default is true.
      */
-    forceSingleModeWidth: boolean,
+    forceSingleModeWidth?: boolean,
     /**
      * (Number) Window resize throttle in milliseconds. Default is 50.
      */
-    throttle: number,
+    throttle?: number,
     /**
      * Auto resize. Adds a listener to the window if true to detect changes.
      * Default: false
      */
-    autoResize : boolean,
+    autoResize?: boolean,
     /**
      * (Function) Will be called when text is fitted.
      */
-    onReady:(finalFontSize: number )=>void,
+    onReady?:(finalFontSize: number )=>void,
     /**
      * Function to apply to the node, when a new interpolated value is checked. 
      * Default: (node, val) => node.style.fontSize = val + "px"
      */
-    style : (node : Node, val : number) => node.style.fontSize = val + "px",
+    style?: (node : Node, val : number) => node.style.fontSize = val + "px",
     /**
      * The width that should be enforced. Use either width & height or parent.
      * If parent is set, this is ignored
      * * Default: 100
      */
-    width : number,
+    width?: number,
     /**
      * The height that should be enforced. Use either width & height or parent.
      * If parent is set, this is ignored
      * Default: 100
      */
-    height : number,
+    height?: number,
     /**
      * Parent. Container, that should be filled.
      * 
@@ -61,7 +61,7 @@ interface TextfitProps {
      *     <p use:textfit={{parent:parentDiv}}>{text}</p>
      *   </div>
      */
-    parent : Node,
+    parent?: Node,
     /**
      * Custom function which checks, if the element fits the width. 
      * Default: (el,width)=>  {
@@ -71,7 +71,7 @@ interface TextfitProps {
      *    return el.offsetWidth - 1 <= width - 1;
      * }
      */
-    elementFitsWidth : (el : Node, width : number) => boolean
+    elementFitsWidth?: (el : Node, width : number) => boolean
     /**
      * Custom function which checks, if the element fits the height. 
      * Default: (el,height)=>  {
@@ -81,7 +81,7 @@ interface TextfitProps {
      *    return el.offsetHeight - 1 <= height - 1;
      * }
      */
-    elementFitsHeight : (el : Node, width : number) => boolean
+    elementFitsHeight?: (el : Node, width : number) => boolean
 }
 
 /**
